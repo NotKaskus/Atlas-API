36492cefe70e0f91d67b91f4965cd2c394923107
NotKaskus <andrewonrubia@gmail.com>
2022-10-03 12:08:53 +0800

    feat: use better-module-alias for relative paths

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
-const favicon = require('serve-favicon');
-const xss = require('xss-clean');
-const path = require('node:path');
-const cors = require('cors');
+const { join } = require('node:path');
+require('better-module-alias')(join(__dirname, '..'));
+const app = require('./app');
 
-const app = express();
-
-app.disable('x-powered-by');
-app.use(xss());
-app.use(cors());
-app.use(helmet({ frameguard: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(favicon(path.join(__dirname, 'assets/images/atlas-logo.png')));
-app.set('trust proxy', 1);
-app.set('json spaces', 2);
-
-app.use('/', require('#routes/index'));
-
-module.exports = app;
+app.listen(4000, () => {
+	console.log('Test Server Started on http://localhost:' + 4000);
+});
