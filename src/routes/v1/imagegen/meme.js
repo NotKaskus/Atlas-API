d3a02e554ff91ed8df99e1375b544b9c2e5e619e
NotKaskus <andrewonrubia@gmail.com>
2022-10-21 08:40:43 +0800

    feat: add change my mind endpoint

--- src/routes/v1/imagegen/meme.js
+++ src/routes/v1/imagegen/meme.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
-const { SonicSays } = require('#controllers/imagegen/meme');
+const { SonicSays, ChangeMyMind } = require('#controllers/imagegen/meme');
 
 router.get('/sonic-says', async (req, res) => {
 	const text = req.query.text;
 
 	try {
 		const image = await SonicSays(text);
+		res.writeHead(200, { 'Content-Type': 'image/jpg' });
+		res.end(image);
+	} catch (err) {
+		// errorResponse(req, res, err.message);
+		console.log(err);
+	}
+});
+
+router.get('/change-my-mind', async (req, res) => {
+	const text = req.query.text;
+
+	try {
+		const image = await ChangeMyMind(text);
 		res.writeHead(200, { 'Content-Type': 'image/jpg' });
 		res.end(image);
 	} catch (err) {
